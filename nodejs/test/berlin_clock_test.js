dcf2977e8cae06f324af38d85928b00024a8d0f7
Alessandro Koterle <alessandro.koterle@gmail.com>
2017-03-12 21:37:25 +0100

    nodejs failing test as kata starting point

--- nodejs/test/berlin_clock_test.js
+++ nodejs/test/berlin_clock_test.js
@@ -1,5 +1,12 @@
-describe("Berlin Clock", function() {
-  it("should ...", function() {
-    expect(berlinClock)
+'use strict'
+
+var chai = require('chai')
+var expect = chai.expect
+var it = require('mocha').it
+var describe = require('mocha').describe
+
+describe('Berlin Clock', function () {
+  it('should ...', function () {
+    expect('fixme').to.equal('!!')
   })
 })
